88911640ca9da1acef38bb5190be30e187641f57
Will <doggy.huang@gmail.com>
2025-06-07 22:21:03 +0800

    fix: 修正輸入檔案名稱為 "input.mp4"

--- scripts/Make_Video.ps1
+++ scripts/Make_Video.ps1
@@ -1,4 +1,4 @@
-ffmpeg -y -i "GitHub Copilot deep dive： Model selection, prompting techniques & agent mode [0Oz-WQi51aU].mkv" `
+ffmpeg -y -i "input.mp4" `
     -vf "subtitles='input.zh.ass'" `
     -c:v h264_nvenc -preset p7 -cq 19 -crf 18 -preset medium -pix_fmt yuv420p `
     -c:a aac -movflags +faststart `
